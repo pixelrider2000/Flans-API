6ba28b73e119ab233da459328bca0a373bd80f34
pixelrider2000 <mullerh311@gmail.com>
2021-09-11 15:42:34 +0200

    Removed debug code

--- commands/TestCommand.java
+++ commands/TestCommand.java
@@ -1,11 +1,8 @@
 package flansapi.commands;
 
-import flansapi.handlers.VehicleHandler;
-import flansapi.main.Main;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.util.ChatComponentText;
 
 public class TestCommand extends CommandBase {
 
@@ -22,23 +19,7 @@ public class TestCommand extends CommandBase {
 	@Override
 	public void processCommand(ICommandSender sender, String[] args) {
 		if(sender instanceof EntityPlayer) {			
-			VehicleHandler vHandler =  new VehicleHandler();
-			EntityPlayer p = (EntityPlayer) sender;	
 			
-			if(args.length == 1) {
-				if(args[0].equalsIgnoreCase("put")) {
-					vHandler.putPlayerInDriveable(p.getUniqueID().toString(), Main.lastSpawned.get(p));
-					p.addChatMessage(new ChatComponentText("You should be sitting now!"));
-				}
-			} else {							
-				int cx = p.getPlayerCoordinates().posX, 
-				cy = p.getPlayerCoordinates().posY, 
-				cz = p.getPlayerCoordinates().posZ;
-							
-				String uuid = vHandler.spawnDriveable(p.getEntityWorld().getWorldInfo().getWorldName(), cx, (cy-1), cz, "vehicle", (cx+15), cy, cz, 90);
-				p.addChatMessage(new ChatComponentText("You spawned a vehicle!"));
-				Main.lastSpawned.put(p, uuid);	
-			}
 		}
 	}
 
