551785f4bb5f6c30703694eb33c012e7fc465bb6
pixelrider2000 <mullerh311@gmail.com>
2021-09-15 16:21:40 +0200

    -

--- commands/TestCommand.java
+++ commands/TestCommand.java
@@ -1,8 +1,11 @@
 package flansapi.commands;
 
+import flansapi.handlers.BulletHandler;
+import flansapi.handlers.ItemHandler;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
 
 public class TestCommand extends CommandBase {
 
@@ -18,8 +21,14 @@ public class TestCommand extends CommandBase {
 
 	@Override
 	public void processCommand(ICommandSender sender, String[] args) {
-		if(sender instanceof EntityPlayer) {			
+		if(sender instanceof EntityPlayer) {	
+			EntityPlayer p = (EntityPlayer) sender;
+			BulletHandler bHandler = new BulletHandler();
+			ItemHandler itemHandler = new ItemHandler();
+			if(p.getHeldItem() == null) return;
+			int id = Item.getIdFromItem(p.getHeldItem().getItem());			
 			
+			System.out.println("Executed!");
 		}
 	}
 
